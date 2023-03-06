b4a78d3583111b84170e986879f9cc97eeee5332
Matthew Taylor <mjt.inbox@gmail.com>
2023-03-06 15:45:29 -0700

    Added a UITableViewController and a ViewController embedded in a navigation controller. Also created an xib file for a custom UITableViewCell.

---
+++ Desserts App/Controllers/DessertsShowViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DessertsShowViewController.swift
+//  Desserts App
+//
+//  Created by Matthew Taylor on 3/6/23.
+//
+
+import UIKit
+
+class DessertsShowViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Desserts App/Views/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Desserts App
+//
+//  Created by Matthew Taylor on 3/6/23.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
